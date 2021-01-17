0a94fa2713849aae8ec1c745a6cc0b263b901428
Jackson Chui <14085209+jacksonchui@users.noreply.github.com>
2021-01-17 14:17:05 -0800

    Project Started

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Jackson Chui on 1/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
